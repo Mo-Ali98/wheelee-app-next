8c7595be084352dd3d64e3cd6eceecb8db1624bc
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2024-07-20 19:53:34 +0100

    setup commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Dashboard() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center"></div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import Link from "next/link";
+
+import AuthButton from "@/components/AuthButton";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -7,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Wheelie",
+  description: "",
 };
 
 export default function RootLayout({
@@ -20,7 +23,21 @@ export default function RootLayout({
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
-          {children}
+          <div className="flex-1 w-full flex flex-col gap-20 items-center">
+            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+                <Link href={"/"}>Wheelie</Link>
+                {/* @ts-expect-error Server Component */}
+                <AuthButton />
+              </div>
+            </nav>
+
+            {children}
+
+            <footer className="w-full border-t border-t-foreground/10 p-3 flex justify-center text-center text-xs">
+              <p>Wheelie</p>
+            </footer>
+          </div>
         </main>
       </body>
     </html>

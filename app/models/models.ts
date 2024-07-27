98fc5e59f959b549212a089dd0a3fae3f565880e
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2024-07-27 20:59:58 +0100

    [FEAT]: add CRUD for accounts table

---
+++ app/models/models.ts
@@ -0,0 +1,8 @@
+export interface Account {
+  id: number;
+  created_at: string;
+  first_name: string;
+  last_name: string;
+  auth_id: string;
+  onboarded: boolean;
+}
---
+++ lib/accounts/accountsService.ts
@@ -0,0 +1,87 @@
+// lib/Accountervice.ts
+
+import { type Account } from "@/app/models/models";
+import { createClient } from "@/utils/supabase/server";
+
+const supabase = createClient();
+
+export async function createAccount(authId: string, username: string) {
+  const { data, error } = await supabase
+    .from("Account")
+    .insert([{ auth_id: authId, username }])
+    .single();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
+export async function getAccounts() {
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+
+  if (authError) {
+    console.error("Error getting user:", authError.message);
+    throw new Error(authError.message);
+  }
+
+  if (!user) {
+    throw new Error("User is not authenticated");
+  }
+
+  const { data, error } = await supabase.from("Account").select("*");
+
+  if (error) {
+    console.error("Error fetching accounts:", error.message);
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
+export async function getAccountByAuthId(authId: string): Promise<Account> {
+  const { data, error } = await supabase
+    .from("Account") // Ensure the table name is correct
+    .select("*")
+    .eq("auth_id", authId); // Filter by authId
+
+  if (error) {
+    console.error("Error fetching account:", error.message);
+    throw new Error(error.message);
+  }
+
+  return data[0];
+}
+
+export async function updateAccount(
+  authId: string,
+  data: Partial<{ username: string }>
+) {
+  const { error } = await supabase
+    .from("Account")
+    .update(data)
+    .eq("auth_id", authId);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return { success: true };
+}
+
+export async function deleteAccount(authId: string) {
+  const { error } = await supabase
+    .from("Account")
+    .delete()
+    .eq("auth_id", authId);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return { success: true };
+}

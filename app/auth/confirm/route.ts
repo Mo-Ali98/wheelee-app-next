019b2fee7745b30ab91c42ff928104fce5790b7c
Mohamed Ahmed <mohamed_ali98@hotmail.co.uk>
2024-08-17 23:27:04 +0100

    clean up

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -6,13 +6,11 @@ import { createClient } from "@/utils/supabase/server";
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
-  const code = searchParams.get("code");
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = searchParams.get("next") ?? "/";
   const redirectTo = request.nextUrl.clone();
   redirectTo.pathname = next;
 
-  console.log(searchParams);
   if (token_hash && type) {
     const supabase = createClient();
 
@@ -20,10 +18,6 @@ export async function GET(request: NextRequest) {
       type,
       token_hash,
     });
-
-    if (type == "recovery" && code) {
-      console.log("recovery");
-    }
 
     if (!error) {
       return NextResponse.redirect(redirectTo);
--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,16 +1,13 @@
-// /app/auth/auth-code-error/page.tsx
-
-import Link from "next/link";
+import AuthButton from "@/components/AuthButton";
 
 export default function AuthCodeError() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold mb-4">Authentication Error</h1>
       <p className="text-lg mb-6">
-        There was a problem with your authentication code. Please try again or
-        contact support if the issue persists.
+        Please try again or contact support if the issue persists.
       </p>
-      <Link href="/login">Go back to Login</Link>
+      <AuthButton />
     </div>
   );
 }
